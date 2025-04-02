d461f2ca27b8c54962c6d12148b0a7e73ddbb7d1
Wahidkherchache <wahidkherchache@gmail.com>
2025-04-02 14:25:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 let thehead = document.querySelector(".outher");
 let thehadbotton = document.querySelector("#outerlinks");
-
+let clos = document.getElementById("close");
 thehadbotton.onclick = function() {
     if(thehead.style.display === "none"){
         thehead.style.display = "block";
     }else{
             thehead.style.display = "none";
     }
+}
+clos.onclick = function() {
+    thehead.style.display = "none";
 }
